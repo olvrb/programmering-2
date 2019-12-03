eb526f4810dd40e0c525c87898907894ac4d205c
Oliver Boudet <oboudet1@me.com>
2019-12-03 22:39:13 +0100

    comments

--- netflix/Elements/Title.cs
+++ netflix/Elements/Title.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using UnogsClient;
 
@@ -18,19 +19,30 @@ namespace netflix.Elements {
         public Title(string name) {
             // Create new Title by fetching data from the Netflix API.
             UnogsClient.UnogsClient client = new UnogsClient.UnogsClient();
+            // Fetch data from unogs api
             ApiItem item = client.GetTitleByName(name);
+            
             Name = item.Title;
-            ratings = new List<Rating> {new Rating(decimal.Parse(item.Rating))};
+            ratings = new List<Rating> {new Rating(decimal.Parse(item.Rating, CultureInfo.InvariantCulture))};
             Thumbnail = Library.ImageFromUrl(item.Image);
             NetflixId = item.NetflixId;
             Description = item.Synopsis;
         }
 
+        // Description of title
         public string Description { get; protected set; }
+
+        // Display name
         public string Name { get; protected set; }
+
+        // Display thumbnail
         public Image Thumbnail { get; protected set; }
+
+        // Netflix ID, allows us to give the user a clickable link
         public string NetflixId { get; protected set; }
 
+
+        // Read-only list, since we don't want anyone to add manually.
         public IReadOnlyList<Rating> Ratings => ratings.AsReadOnly();
 
         public decimal AverageRating => Ratings.Count > 0 ? Ratings.Average(x => x.Value) : 0;
