1a04ff119e2c67f56eeac0a4dc8f9f7a694d5313
Oliver Boudet <oboudet1@me.com>
2020-01-15 14:03:22 +0100

    test

---
+++ Brakmakargatan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    string fighterName;
+
+    [SerializeField]
+    PlayerController opponent;
+    
+    public PlayerController Opponent
+    {
+        private set
+        {
+            opponent = value;
+        }
+        get
+        {
+            return opponent;
+        }
+    }
+
+    private void Start()
+    {
+        Opponent = FindOpponent();
+    }
+
+    public string FighterName
+    {
+        private set
+        {
+            fighterName = value;
+        }
+        get
+        {
+            return fighterName;
+        }
+    }
+
+    public string controlPrefix = "P1";
+
+    public string VerticalAxis
+    {
+        private set { }
+        get
+        {
+            return controlPrefix + "Vertical";
+        }
+    }
+
+    public string HorizontalAxis
+    {
+        private set { }
+        get
+        {
+            return controlPrefix + "Horizontal";
+        }
+    }
+
+    public string PunchAxis
+    {
+        private set { }
+        get
+        {
+            return controlPrefix + "Fire1";
+        }
+    }
+
+    public string KickAxis
+    {
+        private set { }
+        get
+        {
+            return controlPrefix + "Fire2";
+        }
+    }
+
+    public PlayerController FindOpponent()
+    {
+        GameObject[] candidates = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject candidate in candidates)
+        {
+            if (candidate != this.gameObject)
+            {
+                return candidate.GetComponent<PlayerController>();
+            }
+        }
+
+        return this;
+    }
+}
---
+++ NetworkJeff/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    [SerializeField]
+    float movementSpeed = 3f; // Unity-enheter per sekund
+
+    [SerializeField]
+    float rotationSpeed = 150f; // Grader per sekund
+
+    [SerializeField]
+    GameObject bulletPrefab;
+
+    [SerializeField]
+    Transform bulletSpawnPoint;
+
+    float timeBetweenShots = 0.0f;
+    float timeSinceLastShot = 0f;
+	
+	void Update () {
+        if (!isLocalPlayer) return;
+        float yRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+        float zMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 rotationVector = new Vector3(0, yRotation, 0);
+        Vector3 movementVector = new Vector3(0, 0, zMovement);
+
+        transform.Rotate(rotationVector);
+        transform.Translate(movementVector);
+
+        timeSinceLastShot += Time.deltaTime;
+
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            if (timeSinceLastShot > timeBetweenShots)
+            {
+                CmdFire();
+                timeSinceLastShot = 0;
+            }
+        }
+
+	}
+
+    [Command]
+    void CmdFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        NetworkServer.Spawn(bullet);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+}
